32702b1c6aa5da52ad99ecbd3731c537c7fc52f4
Дмитрий <trololodim69@gmail.com>
2023-03-27 20:55:26 +0300

    15 page

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Person tom;
         tom = new Person();
+
+        //
+        tom.name = "Tom";
+        tom.age = 36;
+
         tom.displayInfo();
     }
 }
