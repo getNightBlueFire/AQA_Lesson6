dbe5ec547cfcbc06b2d70e4104f436250bb81d14
Дмитрий <trololodim69@gmail.com>
2023-03-27 21:45:06 +0300

    5-6 line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CreditCard card1 = new CreditCard();
+        CreditCard card2 = new CreditCard();
+        CreditCard card3 = new CreditCard();
+
+        card1.addMoney(100);
+        card2.addMoney(100);
+        card3.getMoney(100);
+
+        card1.display();
+        card2.display();
+        card3.display();
     }
 }
