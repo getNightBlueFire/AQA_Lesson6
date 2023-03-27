2aff86f55a27f9162cf5ed595772341b0827f09a
Дмитрий <trololodim69@gmail.com>
2023-03-27 20:59:27 +0300

    17 -1 page

--- src/Person.java
+++ src/Person.java
@@ -6,5 +6,18 @@ public class Person {
         System.out.printf("Name: %s,\t Age: %d\n", name, age);
     }
 
+    Person() {
+        name = "NoName";
+        age = 18;
+    }
 
+    Person(String n) {
+        name = n;
+        age = 18;
+    }
+
+    Person(String n, int a) {
+        name = n;
+        age = a;
+    }
 }
